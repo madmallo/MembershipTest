fdab36e7a9559976297c24d86bbd13af32310780
Fabio Malaguti <madmallo1987@hotmail.it>
2018-05-18 16:54:31 +0200

    Aggiungere i file di progetto.

---
+++ MembershipTest/Models/ManageViewModels/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MembershipTest.Models.ManageViewModels
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
