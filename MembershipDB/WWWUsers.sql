0a8376ecfcf49d12c4c8ae5047999a68ad58e773
Fabio Malaguti <madmallo1987@hotmail.it>
2018-05-18 17:48:24 +0200

    Creazione

---
+++ MembershipDB/WWWUsers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[WWWUsers]
+(
+	[wwuId] CHAR(9) NOT NULL PRIMARY KEY, 
+    [wwuTipoAnag] CHAR NOT NULL, 
+    [wwuCodiceAnag] CHAR(6) NOT NULL, 
+    [wwuDestinazione] CHAR(3) NOT NULL, 
+    [wwuMagazzino] CHAR(3) NOT NULL
+)
---
+++ MembershipDB/WWWUsersLogin.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[WWWUsersLogin]
+(
+	[wulId] CHAR(9) NOT NULL PRIMARY KEY, 
+    [wulLogin] VARCHAR(30) NOT NULL, 
+    [wulPassword] VARCHAR(15) NOT NULL, 
+    [wulLivello] CHAR(5) NOT NULL, 
+    [wulDataCreazione] DATETIME NOT NULL, 
+    [wulIp] VARCHAR(50) NOT NULL, 
+    [wulFiltroAnag] CHAR(9) NOT NULL, 
+    [ts] TIMESTAMP NOT NULL
+)
